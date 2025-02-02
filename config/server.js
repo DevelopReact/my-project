9967a20024f0c933366a41d004e6056ee812e8b5
DevelopReact <163751366+DevelopReact@users.noreply.github.com>
2025-02-02 15:10:12 +0200

    Fix: Added app.keys in config/server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,21 @@
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS", [
+      "ef81004cbe41272d46d4e493d2a523e39a73cd6f87cf492ed298860484288b25",
+      "a6529669ef2817f89ce5038bb05425dc0a54f3e01894e1d84eb3cafc5d48c103",
+    ]),
   },
 });
